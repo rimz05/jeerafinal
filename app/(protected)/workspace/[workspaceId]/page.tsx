7909f66162ac3b358c9eef228a1cccd0e5c4417f
rimz05 <rimz05@email.com>
2026-08-18 09:41:05 +0530

    vercel fix

--- app/(protected)/workspace/[workspaceId]/page.tsx
+++ app/(protected)/workspace/[workspaceId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { WorkspaceAvatar } from "@/components/workspace/workspace-avatar";
 import { notFound } from "next/navigation";
+import { PROJECT_COLORS } from "./constant";
 
 interface PageProps {
   params: Promise<{
@@ -129,16 +130,3 @@ const WorkspaceHomePage = async ({ params }: PageProps) => {
 };
 
 export default WorkspaceHomePage;
-
-
-
-// for diffrent project colour
-export const PROJECT_COLORS = [
-  "bg-red-100 text-red-700 border-red-200 dark:bg-red-950/40 dark:text-red-400 dark:border-red-900",
-  "bg-blue-100 text-blue-700 border-blue-200 dark:bg-blue-950/40 dark:text-blue-400 dark:border-blue-900",
-  "bg-green-100 text-green-700 border-green-200 dark:bg-green-950/40 dark:text-green-400 dark:border-green-900",
-  "bg-purple-100 text-purple-700 border-purple-200 dark:bg-purple-950/40 dark:text-purple-400 dark:border-purple-900",
-  "bg-yellow-100 text-yellow-700 border-yellow-200 dark:bg-yellow-950/40 dark:text-yellow-400 dark:border-yellow-900",
-  "bg-pink-100 text-pink-700 border-pink-200 dark:bg-pink-950/40 dark:text-pink-400 dark:border-pink-900",
-  "bg-orange-100 text-orange-700 border-orange-200 dark:bg-orange-950/40 dark:text-orange-400 dark:border-orange-900",
-]
